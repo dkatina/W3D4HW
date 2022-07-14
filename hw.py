a15bdbc5a99cc6abca0c55f0ba56014cd2c2c896
Dylan_Katina <dylankatina@gmail.com>
2022-07-14 17:36:18 -0400

    Moving Zeros, and Unittest

---
+++ hw.py
@@ -0,0 +1,32 @@
+# Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of
+# the non-zero elements.
+
+def move_zeros(a_list):
+    zeros = []
+    while 0 in a_list:
+        a_list.remove(0)     #I would think this one is probably the fastest as its a single loop
+        zeros.append(0)
+    return a_list + zeros
+
+
+def move_zeros2(a_list):
+    return list(filter(lambda num: True if num != 0 else False, a_list))\
+        + list(filter(lambda num: True if num == 0 else False, a_list))  # This one is a cleaner version of the first
+
+
+def move_zeros3(a_list):
+    return list(filter(lambda num: True if num != 0 and num != '0' and num != 'zero' and num != 0.0 else False, a_list))\
+        + list(filter(lambda num: True if num == 0 or num == '0' or num == 'zero' or num == 0.0 else False, a_list))      #This one catches ALL zeros 
+
+        #Floats don't matter in the test, but MZ3 Gets them fellas all the same
+
+print(move_zeros3([1,2,0.0,3,0,4]))
+
+         
+
+
+
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,50 @@
+from unittest import TestCase, main
+from hw import move_zeros, move_zeros2, move_zeros3
+
+class MoveZerosTestCase(TestCase):
+
+    def test_1_base(self):
+        self.assertEqual(move_zeros([1, 2, 0, 3, 0, 4]), [1, 2, 3, 4, 0, 0])
+
+    def test_2_double_O(self):
+        self.assertEqual(move_zeros([5, 6, 0, 0, 7, 8]), [5, 6, 7, 8, 0, 0])
+
+    def test_3_starting_with_0(self):
+        self.assertEqual(move_zeros([0, 1, 2, 0, 3, 0, 4]), [1, 2, 3, 4, 0, 0, 0])
+
+    def test_3_ending_with_0(self):
+        self.assertEqual(move_zeros([0, 1, 2, 0, 3, 0, 4, 0, 0]), [1, 2, 3, 4, 0, 0, 0, 0, 0])
+
+    def test_3_starting_with_0(self):
+        self.assertEqual(move_zeros([0, 1, 2, 00, 3, 0, 4]), [1, 2, 3, 4, 0, 00, 0]) 
+
+class MoveZeros3TestCase(TestCase):
+
+    def test_1_base(self):
+        self.assertEqual(move_zeros3([1, 2, 0, 3, 0, 4]), [1, 2, 3, 4, 0, 0])
+
+    def test_2_double_O(self):
+        self.assertEqual(move_zeros3([5, 6, 0, 0, 7, 8]), [5, 6, 7, 8, 0, 0])
+
+    def test_3_starting_with_0(self):
+        self.assertEqual(move_zeros3([0, 1, 2, 0, 3, 0, 4]), [1, 2, 3, 4, 0, 0, 0])
+
+    def test_4_ending_with_0(self):
+        self.assertEqual(move_zeros3([0, 1, 2, 0, 3, 0, 4, 0, 0]), [1, 2, 3, 4, 0, 0, 0, 0, 0])
+
+    def test_5_00(self):
+        self.assertEqual(move_zeros3([0, 1, 2, 00, 3, 0, 4]), [1, 2, 3, 4, 0, 00, 0])
+
+    def test_6_String_0(self):
+        self.assertEqual(move_zeros3(['0', 1, 2, '0', 3, '0', 4]), [1, 2, 3, 4, '0', '0', '0']) 
+
+    def test_7_Zero(self):
+        self.assertEqual(move_zeros3(['zero', 1, 2, 'zero', 3, 'zero', 4]), [1, 2, 3, 4, 'zero', 'zero', 'zero'])
+
+    def test_8_All_Zer0s(self):
+        self.assertEqual(move_zeros3(['0', 1, 2, 'zero', 3, 0, 4]), [1, 2, 3, 4, '0', 'zero', 0])
+                      
+
+
+if __name__ == "__main__":
+    main()
